f1f5112fe4ce75cf1dd19da5adeaf0989627994f
daemon322 <131091382+daemon322@users.noreply.github.com>
2024-10-07 05:54:52 -0500

    Add files via upload

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2,28 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Contratos;
-use Illuminate\Http\Request;
-namespace App\Http\Controllers;
-
-use App\Models\Contratos;
-use App\Models\Productos;
 use App\Models\Clientes;
+use App\Models\Contratos;
 use App\Models\DetallesContratos;
 use App\Models\Observaciones;
 use App\Models\Observacionescontratos;
+use App\Models\Pagos;
+use App\Models\Productos;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
-
-use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Writer\PngWriter;
-
 class ContratosController extends Controller
 {
-   /**
+    /**
      * Display a listing of the resource.
      */
     public function index()
@@ -38,9 +33,9 @@ class ContratosController extends Controller
             [
                 'contratos' => $contratos,
                 'productos' => $Productos,
-                "clientes" => $clientes,
-                "observaciones" => $observaciones,
-                'valor' => "0",
+                'clientes' => $clientes,
+                'observaciones' => $observaciones,
+                'valor' => '0',
             ]
         );
     }
@@ -58,23 +53,28 @@ class ContratosController extends Controller
      */
     public function store(Request $request)
     {
-
+        // Validaciones para el contrato y los pagos
         $request->validate([
-            "clienteid" => "required|exists:clientes,id",
-            "total" => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/"],
-            "descuento" => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/"],
-            "requerimiento" => "required|max:100",
-            "ubicacion" => "required|max:100",
-            "departamento" => "required|max:100",
-            "provincia" => "required|max:100",
-            "distrito" => "required|max:100",
-            "detallecontratos" => "required|array",
-            "detallecontratos.*.cantidad" => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/"],
-            "detallecontratos.*.precio_unitario" => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/"],
-            "detallecontratos.*.subtotal" => ["required", "numeric", "regex:/^\d+(\.\d{1,2})?$/"],
-            "detallecontratos.*.productoid" => "required|exists:productos,id",
+            'clienteid' => 'required|exists:clientes,id',
+            'total' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
+            'descuento' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
+            'requerimiento' => 'required|max:100',
+            'ubicacion' => 'required|max:100',
+            'departamento' => 'required|max:100',
+            'provincia' => 'required|max:100',
+            'distrito' => 'required|max:100',
+            'detallecontratos' => 'required|array',
+            'detallecontratos.*.cantidad' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
+            'detallecontratos.*.precio_unitario' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
+            'detallecontratos.*.subtotal' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
+            'detallecontratos.*.productoid' => 'required|exists:productos,id',
+            'pagos' => 'required|array', // Nueva validación para los pagos
+            'pagos.*.descripcion' => 'required|string|max:255',
+            'pagos.*.cantidad' => ['required', 'numeric', "regex:/^\d+(\.\d{1,2})?$/", 'min:0'],
+            'descuento' => ['nullable', 'numeric', "regex:/^\d+(\.\d{1,2})?$/"],
         ]);
 
+        // Guardar el contrato
         $contratos = new Contratos();
         $contratos->total = $request->total;
         $contratos->requerimiento = $request->requerimiento;
@@ -83,40 +83,48 @@ class ContratosController extends Controller
         $contratos->provincia = $request->provincia;
         $contratos->distrito = $request->distrito;
         $contratos->descuento = $request->descuento;
+        $contratos->total_descuento = $request->total_descuento;
         $contratos->clienteid = $request->clienteid;
 
         $contratos->save();
         $idIngresado = $contratos->id;
 
+        // Guardar los detalles del contrato
         foreach ($request->detallecontratos as $detalle) {
             $detalleCotizacion = new DetallesContratos();
-            $detalleCotizacion->cantidad = $detalle["cantidad"];
-            $detalleCotizacion->precio_unitario = $detalle["precio_unitario"];
-            $detalleCotizacion->subtotal = $detalle["subtotal"];
+            $detalleCotizacion->cantidad = $detalle['cantidad'];
+            $detalleCotizacion->precio_unitario = $detalle['precio_unitario'];
+            $detalleCotizacion->subtotal = $detalle['subtotal'];
             $detalleCotizacion->contratoid = $idIngresado;
-            $detalleCotizacion->productoid = $detalle["productoid"];
+            $detalleCotizacion->productoid = $detalle['productoid'];
             $detalleCotizacion->save();
         }
 
-        $contratos = Contratos::all();
-        $Productos = Productos::all();
-        $clientes = Clientes::all();
-        $observaciones = Observaciones::all();
-
-        foreach($request->obscheck as $obs){
+        // Guardar las observaciones
+        foreach ($request->obscheck as $obs) {
             $obschecks = new Observacionescontratos();
-            $obschecks->observacionesid=$obs;
-            $obschecks->contratosid=$idIngresado;
+            $obschecks->observacionesid = $obs;
+            $obschecks->contratosid = $idIngresado;
             $obschecks->save();
         }
+
+        // Guardar los pagos asociados al contrato
+        foreach ($request->pagos as $pago) {
+            $nuevoPago = new Pagos();
+            $nuevoPago->contrato_id = $idIngresado;
+            $nuevoPago->descripcion = $pago['descripcion'];
+            $nuevoPago->cantidad = $pago['cantidad'];
+            $nuevoPago->save();
+        }
+
         return inertia::render(
             'Contratos/Show',
             [
-                'contratos' => $contratos,
-                'productos' => $Productos,
-                "clientes" => $clientes,
+                'contratos' => Contratos::all(),
+                'productos' => Productos::all(),
+                'clientes' => Clientes::all(),
+                'observaciones' => Observaciones::all(),
                 'valor' => $idIngresado,
-                "observaciones" => $observaciones,
             ]
         );
     }
@@ -128,8 +136,9 @@ class ContratosController extends Controller
     {
         //
         $cotrato = Contratos::findOrFail($id);
+
         return Inertia::render('Contratos/Index', [
-            'cotizacion' => $cotrato
+            'cotizacion' => $cotrato,
         ]);
     }
 
@@ -146,7 +155,16 @@ class ContratosController extends Controller
      */
     public function update(Request $request, Contratos $contratos)
     {
-        //
+        $contratos->total = $request->total;
+        $contratos->total_descuento = $request->total_descuento;
+        $contratos->descuento = $request->descuento;
+        $contratos->requerimiento = $request->requerimiento;
+        $contratos->ubicacion = $request->ubicacion;
+        $contratos->departamento = $request->departamento;
+        $contratos->provincia = $request->provincia;
+        $contratos->distrito = $request->distrito;
+        $contratos->save();
+
     }
 
     /**
@@ -156,7 +174,6 @@ class ContratosController extends Controller
     {
         //
     }
-
 
     public function generarPDF($id)
     {
@@ -168,6 +185,15 @@ class ContratosController extends Controller
         $obscontizaciones = Observacionescontratos::where('contratosid', $id)->get();
         $obs = Observaciones::all();
 
+        // Obtener los pagos asociados al contrato
+        $pagos = Pagos::where('contrato_id', $id)->get();
+
+        // Calcular el total con descuento
+        $totalConDescuento = $contrato->total - $contrato->descuento;
+
+        // Calcular el monto sobrante
+        $montoSobrante = $totalConDescuento - $pagos->sum('cantidad');
+
         $url = route('contratos.pdf', ['id' => $id]);
 
         // Generar el código QR en memoria
@@ -176,10 +202,10 @@ class ContratosController extends Controller
         $result = $writer->write($qrCode);
 
         // Obtener el contenido de la imagen QR
-        $qrCodeDataUrl = 'data:image/png;base64,' . base64_encode($result->getString());
+        $qrCodeDataUrl = 'data:image/png;base64,'.base64_encode($result->getString());
 
         // Renderizar la vista a HTML
-        $html = view('pdf.contratos', compact('contrato', 'detalles', 'productos', 'clientes', 'qrCodeDataUrl','obscontizaciones','obs'))->render();
+        $html = view('pdf.contratos', compact('contrato', 'detalles', 'productos', 'clientes', 'qrCodeDataUrl', 'obscontizaciones', 'obs', 'pagos', 'montoSobrante'))->render();
 
         // Configurar opciones de Dompdf
         $options = new Options();
@@ -197,6 +223,6 @@ class ContratosController extends Controller
         $dompdf->render();
 
         // Devolver el PDF como una respuesta HTTP
-        return $dompdf->stream('cotizacion_' . $id . '.pdf', ['Attachment' => 0]);
+        return $dompdf->stream('cotizacion_'.$id.'.pdf', ['Attachment' => 0]);
     }
 }
--- app/Models/Contratos.php
+++ app/Models/Contratos.php
@@ -9,16 +9,31 @@ class Contratos extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'clienteid',
+        'total',
+        'total_descuento', // Agrega este campo
+        'descuento',
+        'requerimiento',
+        'ubicacion',
+        'departamento',
+        'provincia',
+        'distrito',
+    ];
+
     public function detalles()
     {
         return $this->morphMany(DetallesCotizacion::class, 'detalleable');
     }
+
     // Definición de la relación con el modelo Clientes
     public function cliente()
     {
         return $this->belongsTo(Clientes::class, 'clienteid');
     }
-    public function detallescontrato(){
+
+    public function detallescontrato()
+    {
         return $this->belongsTo(DetallesContratos::class, 'contratoid');
     }
 }
